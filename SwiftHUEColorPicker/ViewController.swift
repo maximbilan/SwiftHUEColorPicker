d77425d01a7461216499525839c30f313bf31b97
Maxim Bilan <maximb.mail@gmail.com>
2015-05-06 13:52:42 +0300

    Added swift project template

---
+++ SwiftHUEColorPicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftHUEColorPicker
+//
+//  Created by Maxim Bilan on 5/6/15.
+//  Copyright (c) 2015 Maxim Bilan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ SwiftHUEColorPickerTests/SwiftHUEColorPickerTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftHUEColorPickerTests.swift
+//  SwiftHUEColorPickerTests
+//
+//  Created by Maxim Bilan on 5/6/15.
+//  Copyright (c) 2015 Maxim Bilan. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SwiftHUEColorPickerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
