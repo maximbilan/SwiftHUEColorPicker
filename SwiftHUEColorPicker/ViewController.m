0ce32931bd597c2354648c6bd07466ffe6f62a69
Maxim Bilan <maximb.mail@gmail.com>
2015-05-06 11:23:53 +0300

    Added project template

---
+++ SwiftHUEColorPicker/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SwiftHUEColorPicker
+//
+//  Created by Maxim Bilan on 5/6/15.
+//  Copyright (c) 2015 Maxim Bilan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
