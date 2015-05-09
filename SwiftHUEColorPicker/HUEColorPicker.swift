90816caf9dc504f32a37709f383977f24929fd31
Maxim Bilan <maximb.mail@gmail.com>
2015-05-09 14:03:32 +0300

    Reworked rendering

--- SwiftHUEColorPicker/HUEColorPicker.swift
+++ SwiftHUEColorPicker/HUEColorPicker.swift
@@ -37,6 +37,7 @@ class HUEColorPicker: UIView {
 	private var color: UIColor!
 	private var currentSelectionY: CGFloat = 0.0
 	private var currentSelectionX: CGFloat = 0.0
+	private var hueImage: UIImage!
 	private var roundedRectPath: UIBezierPath!
 	
 	// Initialization
@@ -56,12 +57,14 @@ class HUEColorPicker: UIView {
 	override func layoutSubviews() {
 		super.layoutSubviews()
 		
-		let image = getImageWithColor(UIColor.yellowColor(), size: self.frame.size)
+		hueImage = getImageWithColor(UIColor.yellowColor(), size: self.frame.size)
 	}
 	
 	func getImageWithColor(color: UIColor, size: CGSize) -> UIImage {
 		var rect = CGRectMake(0, 0, size.width, size.height)
 		UIGraphicsBeginImageContextWithOptions(size, false, 0)
+		
+		UIBezierPath(roundedRect: rect, cornerRadius: 10.0).addClip()
 		
 		if direction == .Horizontal {
 			for var x: Int = 0; x < Int(self.frame.size.width); ++x {
@@ -77,6 +80,11 @@ class HUEColorPicker: UIView {
 				UIRectFill(temp)
 			}
 		}
+		
+//		[UIBezierPath bezierPathWithRoundedRect:imageView.bounds
+//			cornerRadius:10.0] addClip];
+		
+		
 		
 		var image: UIImage = UIGraphicsGetImageFromCurrentImageContext()
 		UIGraphicsEndImageContext()
@@ -108,21 +116,24 @@ class HUEColorPicker: UIView {
 //		var temp = (direction == .Horizontal ? CGRectMake(tempXPlace, 0, 1.0, self.frame.size.height) : CGRectMake(0, tempYPlace, self.frame.size.width, 1.0))
 //		UIRectFill(temp)
 		
-		if direction == .Horizontal {
-			for var x: Int = 0; x < Int(self.frame.size.width); ++x {
-				UIColor(hue: CGFloat(CGFloat(x) / self.frame.size.width), saturation: 1.0, brightness: 1.0, alpha: 1.0).set()
-				let temp = CGRectMake(CGFloat(x), 0, 1, self.frame.size.height)
-				UIRectFill(temp)
-			}
-		}
-		else {
-			for var y: Int = 0; y < Int(self.frame.size.height); ++y {
-				UIColor(hue: CGFloat(CGFloat(y) / self.frame.size.height), saturation: 1.0, brightness: 1.0, alpha: 1.0).set()
-				let temp = CGRectMake(0, CGFloat(y), self.frame.size.width, 1)
-				UIRectFill(temp)
-			}
-		}
+//		if direction == .Horizontal {
+//			for var x: Int = 0; x < Int(self.frame.size.width); ++x {
+//				UIColor(hue: CGFloat(CGFloat(x) / self.frame.size.width), saturation: 1.0, brightness: 1.0, alpha: 1.0).set()
+//				let temp = CGRectMake(CGFloat(x), 0, 1, self.frame.size.height)
+//				UIRectFill(temp)
+//			}
+//		}
+//		else {
+//			for var y: Int = 0; y < Int(self.frame.size.height); ++y {
+//				UIColor(hue: CGFloat(CGFloat(y) / self.frame.size.height), saturation: 1.0, brightness: 1.0, alpha: 1.0).set()
+//				let temp = CGRectMake(0, CGFloat(y), self.frame.size.width, 1)
+//				UIRectFill(temp)
+//			}
+//		}
 		
+		if hueImage != nil {
+			hueImage.drawInRect(rect)
+		}
 		
 		let radius = (direction == .Horizontal ? self.frame.size.height : self.frame.size.width)
 		let circleRect = (direction == .Horizontal ? CGRectMake(currentSelectionX - radius * 0.5, 0, radius, radius) : CGRectMake(0, currentSelectionY - radius * 0.5, radius, radius))
